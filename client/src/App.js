6ff1a239de0ac062c079a9a672266b09e8bfd2a4
dharshan-m <dharshanmohan2704@gmail.com.com>
2023-07-23 22:06:44 +0530

    modified the app apge

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,13 @@ const App = () => {
         const Navigate = useNavigate();
 
         const data= useEffect(() => {
-        const timer = setTimeout(() => Navigate('/login'), 1550);
+        const timer = setTimeout(() => Navigate('/login'), 3000);
         return () => clearTimeout(timer);
     }, []);
 
     return(
         <div onLoad={data}>
-            <h1 className="h1class">Perfect Planners</h1>
+            <img src="https://document-export.canva.com/W2I7I/DAFe7hW2I7I/21/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20230723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230723T132516Z&X-Amz-Expires=15668&X-Amz-Signature=81e31c0c8657011553b3e1e1b1069ddcfec726705fe1d95da1be023619b9eee8&X-Amz-SignedHeaders=host&response-expires=Sun%2C%2023%20Jul%202023%2017%3A46%3A24%20GMT" alt="img" />
         </div>
     )
 }
